164b0986c402427386ace59d36428744839689ed
pushkarv <pushkarv@gmail.com>
2021-01-25 17:48:40 -0500

    updated

--- analyze.sh
+++ analyze.sh
@@ -1,17 +1,39 @@
-#~/bin/sh
+#!/bin/sh
+while getopts ":i:t:" o; do
+    case "${o}" in
+        i)
+            i=${OPTARG}
+            ;;
+        t)
+            t=${OPTARG}
+            ;;
+        *)
+            echo 'Usage: analyze.sh -i <image name> -t <term to grep>'
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-if [$1 == '']; then
-	echo "ERROR - Enter an image name"
+if [ -z "${i}" ] || [ -z "${t}" ]; then
+            echo 'Usage: analyze.sh -i <image name> -t <term to grep>'
 else
 	#save docker image as a tar
-	docker save $1 -o image.tar
-
+	echo "Saving docker image as tar"
+	docker save ${i} -o image.tar
+	
+	rm -rf image
+	echo "Deleting existing image directory"
 	#extract image contents in a directory
+	echo "Making image directory and extracting contents"
 	mkdir image && tar xf image.tar -C image && cd image
 
 	#extract layers' contents
+	echo "Extracting layer contents"
 	for layerDir in $(ls -d */); do
 		mkdir $layerDir/layer && tar xf $layerDir/layer.tar -C $layerDir/layer
 	done
+
+	echo "Searching for ${t}"
+	grep -R -H ${t}
 
 fi
